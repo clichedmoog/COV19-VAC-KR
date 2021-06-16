4a9697a610a6c0aba8a775f131eabd3fdd92c89a
ms.seo <clichedmoog@gmail.com>
2021-06-16 12:34:06 +0900

    Enhances speed; opens browser first

--- check.py
+++ check.py
@@ -1,6 +1,8 @@
 import json
+import random
 import sys
 import time
+import os
 
 from decimal import Decimal
 from urllib.parse import urlparse, parse_qs
@@ -20,7 +22,17 @@ item_url = 'https://v-search.nid.naver.com/reservation/standby?orgCd={cd}&sid={s
 progress_url = 'https://v-search.nid.naver.com/reservation/progress?key={key}&cd={vaccine_id}'
 session = requests.session()
 
+# Colors from https://stackoverflow.com/a/54955094
+class style:
+    ENDC = '\033[0m'
+    OK = '\033[94m'
+    SUCCESS = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+# Check agencies
 def check_agencies(x1='126.8281358', y1='37.5507676', x2='126.8603223', y2='37.5872323'):
     x1 = Decimal(x1)
     y1 = Decimal(y1)
@@ -57,17 +69,22 @@ def check_agencies(x1='126.8281358', y1='37.5507676', x2='126.8603223', y2='37.5
         'Content-Length': str(len(json.dumps(data)))
     }
     r = requests.post(list_url, headers=headers, json=data)
-    items = r.json()[0]['data']['rests']['businesses']['items']
+    try:
+        items = r.json()[0]['data']['rests']['businesses']['items']
+    except TypeError:
+        print(f'\n{style.WARNING}Did not not get correct response from server {r.json()}')
+        return []
     found_items = []
     for item in items:
         v = item['vaccineQuantity']
-        if v.get('quantity') != '0':
+        if v and v.get('quantity') != '0':
             found_items.append(item)
     
     return found_items
 
 
-def view_agency(cd, sid, naver_cookies, vaccine_id, auto_progress=True):
+# Follow redirects and get progress url
+def view_agency(cd, sid, naver_cookies, vaccine_id, driver, auto_progress=False):
     headers = {
         'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
@@ -94,17 +111,67 @@ def view_agency(cd, sid, naver_cookies, vaccine_id, auto_progress=True):
     if elem[0].has_attr('disabled'):
         return None
     else:
+        print(f'\n{style.OK}Found vaccine available: {r.url}')
         if auto_progress:
-            print(r.url)
-            return progress_agency(key, naver_cookies, vaccine_id)
+            print(f'{style.OK}Progressing automatically', ends='')
+            url = progress_url.format(**locals())
+            driver = open_naver_page(driver, url)
+            elem = driver.find_element(By.CLASS_NAME, 'h_title')
+            result_title = elem.get_attribute('textContent')
+            if result_title == '잔여백신 당일 예약이 실패되었습니다.':
+                print(f'{style.WARNING}Failed')
+                return False
+            else:
+                print(f'{style.SUCCESS}Successful')
+                return True
         else:
-            return r.url
+            print(f'\n{style.OK}Found vaccine available: {r.url}')
+            driver = open_naver_page(driver, r.url)
+            return True
 
-
-def progress_agency(key, naver_cookies, vaccine_id):
-    timeout = 10
-    url = progress_url.format(**locals())
+# Opens NAVER page with login using cookies
+def open_naver_page(driver, url):
     print(f'Will open {url}')
+    driver.get(url)
+    driver.switch_to.window(driver.current_window_handle)
+
+
+# Check areas and view_agency when found vaccineQuantity
+def check(area_list, vaccine_id, naver_cookies, driver, auto_progress):
+    for area in area_list:
+        found_items = check_agencies()
+        if len(found_items) > 0:
+            print(f'\n{style.OK}Found {len(found_items)} agencies')
+        for item in found_items:
+            name = item.get('name')
+            print(f'Checking {name} ...')
+            result = view_agency(item['vaccineQuantity']['vaccineOrganizationCode'], item['id'], naver_cookies=naver_cookies, vaccine_id=vaccine_id, driver=driver, auto_progress=auto_progress)
+            if result:
+                return result
+                
+            
+
+
+def main():
+    # Areas to monitor, this is bounds param from NAVER map using links like https://m.place.naver.com/rest/vaccine?vaccineFilter=used&x=126.9731665&y=37.5502763&bounds=126.94098%3B37.5125681%3B127.005353%3B37.5879655
+    areas = [
+        '126.7992413%3B37.5587393%3B126.8355692%3B37.5783834',  # 공항
+        '126.8307895%3B37.5514289%3B126.8671173%3B37.5710749',  # 가양
+        '127.0023489%3B37.4845639%3B127.033248%3B37.5042445'    # 서초~강남
+    ]
+    vaccine_id = 'VEN00015' # 'VEN00015': AZ, 'VEN00016': Janssen
+    naver_cookies = {       # Needs NNB, NID_AUT, NID_JKL, NID_SES cookies
+        'NNB': 'AFQAQGVV37EGA',
+        'NID_AUT': 'CLW9ByqiNqGEbrtG/tG79Qrv3FAtTEb9bTFfW+C8o4d8MmZohL08KYuEZWh/sFRs',
+        'NID_JKL': '8V+/hO68WQfz3DYqgi3eHkt1zvDlouj7hdWvdWvm6hM=',
+        'NID_SES': 'AAABkWYlwtTQDKBbPXGkITDMxjREq1U9CTJhdtiitAL/CDV5NDIJ4nGRcHcC7kI2H6ypeeQjvUBFF45owJdqjrnDyEXOCYQ08RKSTv9PfDVTrM3AzHLX96SMOjEM02CI0JgLQzy/5LAvO0SZ4+EOM/WptrL/Mcnw13JKtrH8A/gfcOdpRjHOFbGHD9/gZl1UkwUv6q1PIuCdwBPqNHlAh3ch6hND8pEgBqT9KFxVGNqL3V9XCh8rB/HC8JJL85YGgify24zC4dZx17v9j53seHnaEVVyjdv2Ag88drkhQULZfq1j9KIq/rbXcZPls9U1hf42WvrSWLbqdHc1fIDIiIuAF2CNrxiI+cwoMN/2c/yhlRnpi1tOHtQf7pPEn0YJ+K4dsFlYvxLNQ4IrybpaV7w+Js1Xdse4yqPMYxPM1DqcAxpMIgrFn68TgKY9Lf+ffOggkfgTlBO+esPH2doCKnHeJ4jycpr7ewE2JjdEBD6YtavLeDdmYuZkAcIViJRdz2OCu3a2KDT+JCH8WPDaqPd8L2NtwpYyGKltqRa2RBsnQ3lN',
+    }
+
+    # Build floats from bounds; separator ";" urlencoded "%3B"
+    area_list = [a.split('%3B') for a in areas] 
+
+    # Login into NAVER
+    timeout = 10
     driver = webdriver.Chrome('./chromedriver')
     wait = WebDriverWait(driver, 10)
     driver.get('https://www.naver.com')
@@ -114,43 +181,21 @@ def progress_agency(key, naver_cookies, vaccine_id):
     driver.add_cookie({'name' : 'NID_AUT', 'value' : naver_cookies.get('NID_AUT'), 'path': '/', 'domain' : '.naver.com'})
     driver.add_cookie({'name' : 'NID_JKL', 'value' : naver_cookies.get('NID_JKL'), 'path': '/', 'domain' : '.naver.com', 'secure': True})
     driver.add_cookie({'name' : 'NID_SES', 'value' : naver_cookies.get('NID_SES'), 'path': '/', 'domain' : '.naver.com'})
-    driver.get(progress_url.format(**locals()))
-    elem = driver.find_element(By.CLASS_NAME, 'h_title')
-    result_title = elem.get_attribute('textContent')
-    if result_title == '잔여백신 당일 예약이 실패되었습니다.':
-        return False
-    else:
-        return True
+    driver.refresh()
 
-
-def check(area_list, vaccine_id, naver_cookies):
-    for area in area_list:
-        found_items = check_agencies()
-        for item in found_items:
-            result = view_agency(item['vaccineQuantity']['vaccineOrganizationCode'], item['id'], naver_cookies=naver_cookies, vaccine_id=vaccine_id)
-            return result
-
-
-def main():
-    # Area list to monitor
-    area_list = [
-        ['126.8281358', '37.5507676', '126.8603223', '37.5872323']
-    ]
-    vaccine_id = 'VEN00015' # 'VEN00015': AZ, 'VEN00016': Janssen
-    naver_cookies = {       # Needs NNB, NID_AUT, NID_JKL, NID_SES cookies
-        'NNB': 'AFQAQGVV37EGA',
-        'NID_AUT': 'CLW9ByqiNqGEbrtG/tG79Qrv3FAtTEb9bTFfW+C8o4d8MmZohL08KYuEZWh/sFRs',
-        'NID_JKL': '8V+/hO68WQfz3DYqgi3eHkt1zvDlouj7hdWvdWvm6hM=',
-        'NID_SES': 'AAABkWYlwtTQDKBbPXGkITDMxjREq1U9CTJhdtiitAL/CDV5NDIJ4nGRcHcC7kI2H6ypeeQjvUBFF45owJdqjrnDyEXOCYQ08RKSTv9PfDVTrM3AzHLX96SMOjEM02CI0JgLQzy/5LAvO0SZ4+EOM/WptrL/Mcnw13JKtrH8A/gfcOdpRjHOFbGHD9/gZl1UkwUv6q1PIuCdwBPqNHlAh3ch6hND8pEgBqT9KFxVGNqL3V9XCh8rB/HC8JJL85YGgify24zC4dZx17v9j53seHnaEVVyjdv2Ag88drkhQULZfq1j9KIq/rbXcZPls9U1hf42WvrSWLbqdHc1fIDIiIuAF2CNrxiI+cwoMN/2c/yhlRnpi1tOHtQf7pPEn0YJ+K4dsFlYvxLNQ4IrybpaV7w+Js1Xdse4yqPMYxPM1DqcAxpMIgrFn68TgKY9Lf+ffOggkfgTlBO+esPH2doCKnHeJ4jycpr7ewE2JjdEBD6YtavLeDdmYuZkAcIViJRdz2OCu3a2KDT+JCH8WPDaqPd8L2NtwpYyGKltqRa2RBsnQ3lN',
-    }
-    print('Checking', end='')
+    # System call
+    os.system('')
+    print(f'Monitoring {style.BOLD}{len(area_list)} areas{style.ENDC}')
     sys.stdout.flush()
     result = None
     while not result:
         print('.', end='')
         sys.stdout.flush()
-        result = check(area_list, vaccine_id, naver_cookies)
-        time.sleep(3)
+        result = check(area_list, vaccine_id, naver_cookies, driver=driver, auto_progress=False)
+        if result:
+            input(f'{style.SUCCESS}Waiting for user input')
+        else:
+            time.sleep(0.6 + random.random()) # Check every 1~2 sec
 
 
 
